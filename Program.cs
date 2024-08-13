61c99f2763e118de06b95d6dfada30d3dd3e10f6
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-08-13 16:38:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿
+public class program
+{
+
+    public static void Main(string[] args)
+    {
+
+        //1 - 10 adet tam sayı verisi alacak bir dizi tanımlayınız.
+        int[] numbers = new int[10];
+
+        //2 - Bu dizinin elemanlarını bir for döngüsü ile doldurup, foreach döngüsü ile tekrar ekrana yazınız.
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i;
+        }
+
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+
+        //3 - Bu diziye kullanıcıdan alınan yeni bir değeri ekleyiniz (11. eleman olarak)
+
+        Console.Write("Please enter a number :");
+        int newNumberToAdd = Convert.ToInt32(Console.ReadLine());
+
+        int[] newArray = new int[numbers.Length + 1];
+
+        newArray[numbers.Length] = newNumberToAdd;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            newArray[i] = i;
+        }
+
+        foreach (int i in newArray)
+        {
+            Console.WriteLine(i);
+        }
+
+        //4 - Bu diziyi büyükten küçüğe ekrana yazdırınız.
+
+        Array.Sort(newArray);
+        Array.Reverse(newArray);
+
+        Console.WriteLine("New Array sorted from big to small:");
+
+        foreach (int i in newArray)
+        {
+
+            Console.WriteLine(i);
+
+              }
+
+         }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
